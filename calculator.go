7146c8b0670fc9ff587eae1c03ea5702a6169998
ihsan606 <ihsansyafiul@gmail.com>
2023-02-18 17:42:42 +0700

    add some functionality in calculation

--- calculator.go
+++ calculator.go
@@ -3,3 +3,15 @@ package go_calculator
 func CountPlus(a int, b int) int {
 	return a * b
 }
+
+func CountMinus(a int, b int) int {
+	return a - b
+}
+
+func CountTimes(a int, b int) int {
+	return a * b
+}
+
+func CountDevide(a int, b int) int {
+	return a / b
+}
